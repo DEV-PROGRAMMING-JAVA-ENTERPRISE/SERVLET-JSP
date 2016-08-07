c3a62eea79d0c0137506034e17290e70f16dc69d
Nishant Handa <nh124001@gmail.com>
2016-08-07 21:09:49 +0530

    added lifecycle/PractiseServlet.java

---
+++ Code/ServletExample/src/org/nishant/servlet/lifecycle/PractiseServlet.java
@@ -0,0 +1,51 @@
+package org.nishant.servlet.lifecycle;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PractiseServlet
+ */
+public class PractiseServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public PractiseServlet() {
+
+		super();
+		System.out.println("PractiseServlet default constructor has called");
+	}
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+
+		System.out
+				.println("PractiseServlet init method has called, ServletConfig "
+						+ config);
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("PractiseServlet doGet method has called");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("PractiseServlet destroy method has called");
+	}
+}
